a867bf426ad205b65e4e43a698f7471e36bea09f
iocn-2-3-4 <iocn234@ukr.net>
2017-03-07 21:12:38 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -2,13 +2,14 @@ pipelineJob('example') {
   scm {
     git ('https://github.com/pingwin4ik/dsl.git')
   }
+    triggers {
+     scm('H/2 * * * *')
+  } 
      steps {
     dsl {
       external('jenkinsfile')  
-      // default behavior
-      // removeAction('IGNORE')      
-      removeAction('DELETE')
     }
   }
+
 
 }
