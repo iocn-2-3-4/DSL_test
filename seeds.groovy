fe5ff32587d2f1497760d79ed47e74e15da37495
iocn-2-3-4 <iocn234@ukr.net>
2017-03-07 20:54:10 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -1,14 +1,8 @@
-job('Seeds_test') {
-  scm {
-    git ('https://github.com/iocn-2-3-4/DSL_test.git')
-  }
-    steps {
-    dsl {
-      external('jenkinsfile')  
-      // default behavior
-      // removeAction('IGNORE')      
-      removeAction('DELETE')
+pipelineJob('example') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('project-a-workflow.groovy'))
+            sandbox()
+        }
     }
-
-  }
 }
