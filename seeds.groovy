c1b9897013a1ca8dd88b4f0b28b3a84d64ab85e7
iocn-2-3-4 <iocn234@ukr.net>
2017-03-07 20:29:35 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -2,6 +2,13 @@ job('Seeds_test') {
   scm {
     git ('https://github.com/iocn-2-3-4/git_1.git')
   }
+    steps {
+    dsl {
+      external('jenkinsfile')  
+      // default behavior
+      // removeAction('IGNORE')      
+      removeAction('DELETE')
+    }
 
   }
 }
