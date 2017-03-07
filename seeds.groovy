25281f24cfd4dcee67b0b77396c85ae001b64bd6
iocn-2-3-4 <iocn234@ukr.net>
2017-03-07 20:57:39 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -1,3 +1,22 @@
 pipelineJob('example') {
+     agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
 
 }
