f66843129216edd4cb42517304dc7ad6a9c05cfa
iocn-2-3-4 <iocn234@ukr.net>
2017-03-07 20:28:00 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -2,33 +2,6 @@ job('Seeds_test') {
   scm {
     git ('https://github.com/iocn-2-3-4/git_1.git')
   }
-  steps {
-    pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-        stage('Misc') {
-            steps {
-                echo 'None'
-            }
-        }
-    }
-}
 
   }
 }
