29abdf3987abcda8df8cb1b24287a42b83614135
iocn-2-3-4 <iocn234@ukr.net>
2017-03-14 17:54:21 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -1,3 +1,20 @@
+job('example') {
+    logRotator(-1, 10)
+    jdk('Java 6')
+    scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        gradle('clean build')
+    }
+    publishers {
+        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+    }
+}
+
 pipelineJob('pipeline') {
   scm {
     git ('https://github.com/pingwin4ik/dsl.git')
