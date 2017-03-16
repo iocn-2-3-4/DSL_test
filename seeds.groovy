0f526355caa6d2d247936186ac08a84f92c47fe6
iocn-2-3-4 <iocn234@ukr.net>
2017-03-16 06:28:41 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -1,18 +1,6 @@
 job('example') {
     description('My first job')
     displayName('example')
-    environmentVariables {
-        script('''
-           mkdir -p tests
-           rm -rf /tmp/tests.tmp
-        ''')
-    }
-    steps {
-        groovyScriptFile('generateReports.groovy') {
-            groovyInstallation('groovy-2.4.2')
-            scriptParam('target/reports')
-        }
-    }
 }
 
 pipelineJob('pipeline') {
