d25f27af5a0b998039d1ade250dabdce22c8ad3f
iocn-2-3-4 <iocn234@ukr.net>
2017-03-24 07:41:07 +0200

    Update hello.groovy

--- hello.groovy
+++ hello.groovy
@@ -1,9 +1,1 @@
-class GroovyPrintExample {
-   static void main(String[] args) {
-      print "Hello World"
-      print "World"
-   }
-}
-def a = new GroovyPrintExample()
-a()
 println "Hello World"
